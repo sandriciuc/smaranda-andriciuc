662b86a53988c19d77b9c13c458f35efdf2f043b
Smaranda Andriciuc <smaranda@andriciuc.com>
2026-04-16 10:50:26 +0200

    warning

--- app/layout.tsx
+++ app/layout.tsx
@@ -61,38 +61,39 @@ export default function RootLayout({
 }>) {
   return (
    <html lang="ro" className={`${cormorant.variable} ${dmSans.variable} ${ibmPlexMono.variable}`}>
-      
-      {/* Google Analytics */}
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-STXYCN5NB4"
-        strategy="afterInteractive"
-      />
-      <Script id="ga-script" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-STXYCN5NB4');
-        `}
-      </Script>
+      <body>
+        {children}
 
-      {/* Meta Pixel */}
-      <Script id="fb-pixel" strategy="afterInteractive">
-        {`
-          !function(f,b,e,v,n,t,s)
-          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-          n.queue=[];t=b.createElement(e);t.async=!0;
-          t.src=v;s=b.getElementsByTagName(e)[0];
-          s.parentNode.insertBefore(t,s)}(window,document,'script',
-          'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '1594215188347899');
-          fbq('track', 'PageView');
-        `}
-      </Script>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-STXYCN5NB4"
+          strategy="afterInteractive"
+        />
+        <Script id="ga-script" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-STXYCN5NB4');
+          `}
+        </Script>
 
-      <body>{children}</body>
+        {/* Meta Pixel */}
+        <Script id="fb-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window,document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1594215188347899');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+      </body>
     </html>
   )
 }
